2c6eb789e5df79f28ef0914f1900552613ff6291
dargyris <diamantisargiris@gmail.com>
2020-05-03 21:56:16 +0300

    Basic cpp programs.

---
+++ 00_Introduction/c1_FavoriteNumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Hello!";
+    cout << "What's your favorite number?";
+    int myInt = 0;
+    cin >> myInt;
+    cout << "Amazing!!" << myInt << "'s my favorite number too!\n";
+
+    return 0;
+
+}
