b139ab8aa320aa00b6ed1678ab566a23ada32cc4
dargyris <diamantisargiris@gmail.com>
2020-05-03 21:49:41 +0300

    A simple cpp program.

---
+++ 00_Introduction/01_Intro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+    
+    system( "clear" );
+
+    std::cout << "Enter your favorite number between 1 and 100: ";
+    int myInt = 0;
+
+    std::cin >> myInt;
+    std::cout << "\nAmazing! That's my favorite number too!\n";
+    std::cout << "Testing out autocomplete feature for cpp files.\n";
+    std::cout << "Seems to be working awesome!\n";
+
+    return 0;
+
+}
