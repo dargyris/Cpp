516b4c7f3e3ecd09f832eef07334b46a2357d3e4
dargyris <diamantisargiris@gmail.com>
2020-05-04 10:15:53 +0300

    Introductory cpp program.

---
+++ 00_Introduction/a02_Input.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+
+    int myInt = 0;
+    double myDouble = 0.;
+
+    cout << "Enter an int: ";
+    cin >> myInt;
+
+    cout << "Enter a double: ";
+    cin >> myDouble;
+
+    cout << "The int is: " << myInt << endl;
+    cout << "The double is: " << myDouble << endl;
+
+    return 0;
+
+}
