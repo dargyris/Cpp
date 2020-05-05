b22022d29fb3269a56b76f611c63b5be41c62b31
dargyris <diamantisargiris@gmail.com>
2020-05-05 18:44:12 +0300

    Simple demo of Arrays in c++.

---
+++ 00_Introduction/a14_Arrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+
+    system( "clear" );
+
+    char vowels[] {'a', 'e', 'i', 'o', 'u'};
+    cout << "\nThe first vowel is: " << vowels[0] << endl;
+    cout << "The last vowel is: " << vowels[4] << endl;
+
+}
