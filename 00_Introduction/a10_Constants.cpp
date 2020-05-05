9c89bd6e30bf6c10f80c255f24a99aae15667515
dargyris <diamantisargiris@gmail.com>
2020-05-05 14:49:41 +0300

    Constant types in C++.

---
+++ 00_Introduction/a10_Constants.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+
+    cout << "Types of constants in C++:" << endl;
+    cout << "==========================" << endl;
+    cout << "1. Literal constants" << endl;
+    cout << "2. Declared constants" << endl;
+    cout << "3. Constant expressions" << endl;
+    cout << "4. Enumerated constants" << endl;
+    cout << "5. Defined constants" << endl;
+
+
+    cout << endl;
+    return 0;
+
+}
