4a36f7e3b025f6d40dc0db6445ff3931c975ada2
dargyris <diamantisargiris@gmail.com>
2020-05-08 19:30:21 +0300

    Updating naming style

---
+++ 00_Introduction/a05_UninitVars.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+
+    system( "clear" );
+    cout << "Uninitialized variables:" << endl;
+    int age;
+    cout << "age: " << age << endl;
+
+    return 0;
+
+}
