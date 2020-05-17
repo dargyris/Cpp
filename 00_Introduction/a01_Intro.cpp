0e21f1458542391221e9a95bad57b58243d2ad1d
dargyris <diamantisargiris@gmail.com>
2020-05-17 20:31:35 +0300

    Testing new RegExp for Vim compile.

--- 00_Introduction/a01_Intro.cpp
+++ 00_Introduction/a01_Intro.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
 
-int main(int argc, char *argv[]){
-    
-    system( "clear" );
+int main(){
 
-    std::cout << "Enter your favorite number between 1 and 100: ";
-    int myInt = 0;
-
-    std::cin >> myInt;
-    std::cout << "\nAmazing! That's my favorite number too!\n";
-    std::cout << "Testing out autocomplete feature for cpp files.\n";
-    std::cout << "Seems to be working awesome!\n";
+    std::cout << "Hello world!" << std::endl;
 
     return 0;
 
