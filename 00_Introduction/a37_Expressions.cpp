6c10011b6cd53ac31f34ec1f7f789ecd585a2b6e
dargyris <diamantisargiris@gmail.com>
2020-05-20 01:11:58 +0300

    General C++ concepts.

---
+++ 00_Introduction/a37_Expressions.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+
+    cout << "Expressions are the building blocks of statements." << endl;
+    cout << "Statements are the building blocks of programming." << endl;
+    cout << "Eg.:\tVariable declaration statements" << endl;
+    cout << "\tIO Statements" << endl;
+    cout << "\tSimple assignment statements" << endl;
+    cout << "\tReturn statements" << endl;
+    
+    cout << "\n\t~ Expression definition in C++ ~" << endl;
+    cout << "\"A sequence of operators and operands that specifies a computation.\"" << endl;
+    cout << "\t• If statements contain statements" << endl;
+    cout << "\t• ; is the null statement. It performs no action." << endl;
+    cout << endl;
+    cout << "It is possible to add numbers within the code and do nothing with them, " 
+        << "but this will generate warnings and it is obviously pointless." << endl;
+
+    return 0;
+
+}
