28e676df94d5fdd41504b07e728f48cc04ed5173
dargyris <diamantisargiris@gmail.com>
2020-05-19 11:07:04 +0300

    Simple exercise with Arrays.

---
+++ 00_Introduction/a22_Arrays.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main(){
+
+    //system( "clear" );
+    
+    string myStringArrayA[1] = {"January"};
+    string myStringArrayB[1] = {"February"};
+    string myStringArrayC[1] = {"March"};
+    string myStringArrayD[1] = {"April"};
+    string myStringArrayE[1] = {"May"};
+    string myStringArrayF[1] = {"June"};
+    string myStringArrayG[1] = {"July"};
+    string myStringArrayH[1] = {"August"};
+    string myStringArrayI[1] = {"September"};
+    string myStringArrayJ[1] = {"October"};
+    string myStringArrayK[1] = {"November"};
+    string myStringArrayL[1] = {"December"};
+
+    cout << "myStringArrayA[1]: " << myStringArrayA[0] << endl;
+    cout << "myStringArrayB[1]: " << myStringArrayB[0] << endl;
+    cout << "myStringArrayC[1]: " << myStringArrayC[0] << endl;
+    cout << "myStringArrayD[1]: " << myStringArrayD[0] << endl;
+    cout << "myStringArrayE[1]: " << myStringArrayE[0] << endl;
+    cout << "myStringArrayF[1]: " << myStringArrayF[0] << endl;
+    cout << "myStringArrayG[1]: " << myStringArrayG[0] << endl;
+    cout << "myStringArrayH[1]: " << myStringArrayH[0] << endl;
+    cout << "myStringArrayI[1]: " << myStringArrayI[0] << endl;
+    cout << "myStringArrayJ[1]: " << myStringArrayJ[0] << endl;
+    cout << "myStringArrayK[1]: " << myStringArrayK[0] << endl;
+    cout << "myStringArrayL[1]: " << myStringArrayL[0] << endl;
+
+    return 0;
+}
