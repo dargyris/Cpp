1495b78689a8dc75826cf753670ab5293b86a14f
dargyris <diamantisargiris@gmail.com>
2020-05-19 16:44:21 +0300

    Comparisons with java.

---
+++ 00_Introduction/Main.java
@@ -0,0 +1,23 @@
+public class Main{
+
+    public static void main ( String[] args ){
+
+        System.out.print("\033[H\033[2J");
+
+        int testScore_1 = 0;
+        System.out.println( "testScore_1: " + testScore_1 );
+        int testScore_2 = 0;
+        System.out.println( "testScore_2: " + testScore_2 );
+
+        int[] myArray_1 = new int[5];
+        printArray(myArray_1);
+
+    }
+
+    public static void printArray( int[] array ){
+        for( int i = 0; i < array.length; i++ ){
+            System.out.println( "Element [" + i + "]\t: " + array[i] );
+        }
+    }
+
+}
