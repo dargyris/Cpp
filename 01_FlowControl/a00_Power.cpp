d172c5193dbbea2204925c8032894a7b5a2e7ed9
dargyris <diamantisargiris@gmail.com>
2020-05-20 16:28:30 +0300

    SEQUENCE, SELECTION, ITERATION.

---
+++ 01_FlowControl/a00_Power.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+
+    cout << "With:" << endl;
+    cout << "\nSEQUENCE\n\tSELECTION and\n\t\tITERATION" << endl;
+    cout << "\nWe can do ANYTHING." << endl;
+    
+    cin.get();
+    return 0;
+
+}
