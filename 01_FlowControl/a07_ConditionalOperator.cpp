17b98d0ffe5537eaa807a498ec73e3ab95e72db9
dargyris <diamantisargiris@gmail.com>
2020-05-21 15:01:00 +0300

    Housekeeping.

--- 01_FlowControl/a07_ConditionalOperator.cpp
+++ 01_FlowControl/a07_ConditionalOperator.cpp
@@ -5,12 +5,20 @@ using std::cin;
 using std::endl;
 
 int main(){
-
     system( "clear" );
+
+    for( int i {0}; i<2; ++i ){
+        cout << ( ( i==0 )? "": "\n" ) << "i: " << i;
+        cout << ( ( i==1 )? "\n": "");
+//        ( ( i==0 )? cout << "i: " << i: cout << "\ni: " << i );
+    }
+
+    cout << endl;
     int myIntA { 10 };
     int myIntB { 20 };
     cout << ( ( myIntA < myIntB ) ? 
-            "I am now going through my 6th career change!\nBoy, I'd better make this one work!" 
+            "I am now going through my 6th career change!"
+            "\nBoy, I'd better make this one work!" 
             : "Shouldn't be here..." );
     
     int num {};
