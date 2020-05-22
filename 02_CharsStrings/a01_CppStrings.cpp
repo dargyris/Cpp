607ffc2bc66fa3108d6f6410d9572d5ddb62fc69
dargyris <diamantisargiris@gmail.com>
2020-05-22 21:34:11 +0300

    Spp Strings intro.

---
+++ 02_CharsStrings/a01_CppStrings.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+
+int main(){
+    system("clear");
+
+    cout << std::boolalpha;
+    string s1 {"aa"};
+    string s2 {"a"};
+    cout << (s1.find("z")) << endl;
+    
+    
+//    char c1[] {"aa"};
+//    char c2[] {"a"};
+//    cout << strcmp(c1, c2) << endl;
+//    char myChar = 'a';
+//    cout << static_cast<int>(myChar) << endl;
+
+
+    //cin.get();
+    cin.get();
+    return 0;
+}
