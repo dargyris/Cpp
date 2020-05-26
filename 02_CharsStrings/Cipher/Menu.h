89ff8ee65e1145e5d76a24710cd2710aa1b1b55f
dargyris <diamantisargiris@gmail.com>
2020-05-26 20:50:15 +0300

    Porting menu handling to c++.

--- 02_CharsStrings/Cipher/Menu.h
+++ 02_CharsStrings/Cipher/Menu.h
@@ -4,17 +4,17 @@ char printMenu( unsigned short* );
 
 char printMenu ( unsigned short* threeTries ){
     logo();
-    printf("\n\t\t~ Welcome! ~" );
-    printf("\n\n\tWhat would you like to do?" );
-    printf("\n\t1. Encrypt Message" );
-    printf("\n\t2. Decipher Message" );
-    printf("\n\t3. Choose Cypher" );
-    printf("\n\tQ. Quit" );
-    printf("\n\n\tOnly single character choices are accepted.");
+    cout << "\t\t~ Welcome! ~" << endl;
+    cout << "\n\tWhat would you like to do?" << endl;
+    cout << "\t1. Encrypt Message" << endl;
+    cout << "\t2. Decipher Message" << endl;
+    cout << "\t3. Choose Cypher" << endl;
+    cout << "\tQ. Quit" << endl;
+    cout << endl;
     if ( (*threeTries) < 3 ){
-        printf( "\n\t%hu/3 tries remaining", (*threeTries) );
+        cout <<  "\t" << (*threeTries) << "/3 tries remaining" << endl;
     }
-    printf("\n\t> ");
+    cout << "\t> ";
     char choice {};
     choice = read();
     return choice;
@@ -22,14 +22,14 @@ char printMenu ( unsigned short* threeTries ){
 
 void logo (){
     system("clear");
-    printf("\n\n\n");
-    printf("\n\t       _       _                   " );
-    printf("\n\t      (_)     | |                  " );
-    printf("\n\t   ___ _ _ __ | |__   ___ _ __     " );
-    printf("\n\t  / __| | '_ \\| '_ \\ / _ \\ '__| \u2122" );
-    printf("\n\t | (__| | |_) | | | |  __/ |       " );
-    printf("\n\t  \\___|_| .__/|_| |_|\\___|_|     " );
-    printf("\n\t        | |                       " );
-    printf("\n\t        |_|                       " );
-    printf( "\n" );
+    cout << "\n\n" << endl;
+    cout << "\t       _       _                   " << endl;
+    cout << "\t      (_)     | |                  " << endl;
+    cout << "\t   ___ _ _ __ | |__   ___ _ __     " << endl;
+    cout << "\t  / __| | '_ \\| '_ \\ / _ \\ '__| \u2122" << endl;
+    cout << "\t | (__| | |_) | | | |  __/ |       " << endl;
+    cout << "\t  \\___|_| .__/|_| |_|\\___|_|     " << endl;
+    cout << "\t        | |                       " << endl;
+    cout << "\t        |_|                       " << endl;
+    cout <<  "" << endl;
 }
