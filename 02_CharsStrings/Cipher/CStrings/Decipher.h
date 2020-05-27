b96c27c9ef3c9aa8d801fe1093f4be57d60fbeb8
dargyris <diamantisargiris@gmail.com>
2020-05-27 15:11:52 +0300

    Improvement to the base68 encryption algorithm.

--- 02_CharsStrings/Cipher/CStrings/Decipher.h
+++ 02_CharsStrings/Cipher/CStrings/Decipher.h
@@ -1,7 +1,7 @@
 unsigned short printAll(char[], char[], char type);
 void print( char[] );
 void decipher( char[], char );
-void debase52( char[] );
+void debase68( char[] );
 
 
 unsigned short printAll(char original[], char encrypted[], char type) {
@@ -68,8 +68,8 @@ void decipher( char myString[], char type ) {
                 memset( (myString + strlen(myString)/2), '\0', strlen(myString)/2 );
             }
             break;
-        case '3':     // To Base-24 
-            debase52(myString);
+        case '3':     // To Base-68 
+            debase68(myString);
             break;
         default:
             printf( "Panic!" );
@@ -77,174 +77,14 @@ void decipher( char myString[], char type ) {
     }
 }
 
-void debase52 ( char myString[] ){
+void debase68 ( char myString[] ){
+    char inCode[] {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:!?"};
+    char outCode[] {" .,:0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz~`"};
     for( int i {0}; i<strlen(myString); ++i ){
-        if ( myString[i] >= '0' && myString[i] <= '9' ){
-            continue;
-        } else {
-            switch ( myString[i] ) {
-                case '1':
-                    myString[i] =  'a';
-                    break;
-                case '2':
-                    myString[i] =  'b';
-                    break;
-                case '3':
-                    myString[i] =  'c';
-                    break;
-                case '4':
-                    myString[i] =  'd';
-                    break;
-                case '5':
-                    myString[i] =  'e';
-                    break;
-                case '6':
-                    myString[i] =  'f';
-                    break;
-                case '7':
-                    myString[i] =  'g';
-                    break;
-                case '8':
-                    myString[i] =  'h';
-                    break;
-                case '9':
-                    myString[i] =  'i';
-                    break;
-                case 'a':
-                    myString[i] =  'j';
-                    break;
-                case 'b':
-                    myString[i] =  'k';
-                    break;
-                case 'c':
-                    myString[i] =  'l';
-                    break;
-                case 'd':
-                    myString[i] =  'm';
-                    break;
-                case 'e':
-                    myString[i] =  'n';
-                    break;
-                case 'f':
-                    myString[i] =  'o';
-                    break;
-                case 'g':
-                    myString[i] =  'p';
-                    break;
-                case 'h':
-                    myString[i] =  'q';
-                    break;
-                case 'i':
-                    myString[i] =  'r';
-                    break;
-                case 'j':
-                    myString[i] =  's';
-                    break;
-                case 'k':
-                    myString[i] =  't';
-                    break;
-                case 'l':
-                    myString[i] =  'u';
-                    break;
-                case 'm':
-                    myString[i] =  'v';
-                    break;
-                case 'n':
-                    myString[i] =  'w';
-                    break;
-                case 'o':
-                    myString[i] =  'x';
-                    break;
-                case 'p':
-                    myString[i] =  'y';
-                    break;
-                case 'q':
-                    myString[i] =  'z';
-                    break;
-                case 'r':
-                    myString[i] =  'A';
-                    break;
-                case 's':
-                    myString[i] =  'B';
-                    break;
-                case 't':
-                    myString[i] =  'C';
-                    break;
-                case 'u':
-                    myString[i] =  'D';
-                    break;
-                case 'v':
-                    myString[i] =  'E';
-                    break;
-                case 'w':
-                    myString[i] =  'F';
-                    break;
-                case 'x':
-                    myString[i] =  'G';
-                    break;
-                case 'y':
-                    myString[i] =  'H';
-                    break;
-                case 'z':
-                    myString[i] =  'I';
-                    break;
-                case '!':
-                    myString[i] =  'J';
-                    break;
-                case '@':
-                    myString[i] =  'K';
-                    break;
-                case '#':
-                    myString[i] =  'L';
-                    break;
-                case '$':
-                    myString[i] =  'M';
-                    break;
-                case '%':
-                    myString[i] =  'N';
-                    break;
-                case '^':
-                    myString[i] =  'O';
-                    break;
-                case '&':
-                    myString[i] =  'P';
-                    break;
-                case '*':
-                    myString[i] =  'Q';
-                    break;
-                case '(':
-                    myString[i] =  'R';
-                    break;
-                case ')':
-                    myString[i] =  'S';
-                    break;
-                case '-':
-                    myString[i] =  'T';
-                    break;
-                case '_':
-                    myString[i] =  'U';
-                    break;
-                case '=':
-                    myString[i] =  'V';
-                    break;
-                case '+':
-                    myString[i] =  'W';
-                    break;
-                case '[':
-                    myString[i] =  'X';
-                    break;
-                case ']':
-                    myString[i] =  'Y';
-                    break;
-                case '{':
-                    myString[i] =  'Z';
-                    break;
-                case '~':
-                    myString[i] =  ' ';
-                    break;
-                case '`':
-                    myString[i] =  '.';
-                    break;
+        for( int j {0}; j<strlen(inCode); ++j ){
+            if ( myString[i] == outCode[j] ){
+                myString[i] = inCode[j];
+                break;
             }
         }
     }
