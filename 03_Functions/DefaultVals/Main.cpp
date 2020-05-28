fb819f73f17b14a525bc83f165e69f01ab0e2db1
dargyris <diamantisargiris@gmail.com>
2020-05-28 23:37:05 +0300

    Testing vimscript additions.

---
+++ 03_Functions/DefaultVals/Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip> =
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+double calc_cost( double base_cost, double tax_rate = 0.06, double shipping = 3.50 );
+
+int main(){
+    system("clear");
+
+    
+
+    fflush(stdin);
+    cin.get();
+    return 0;
+}
+
+double calc_cost( double base_cost, double tax_rate = 0.06, double shipping = 3.50 ){
+    return base_cost += ( base_cost * tax_rate ), 
+}
