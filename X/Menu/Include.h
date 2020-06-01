502feec1e0d44fd9f1279cd38811c94ea933d5b5
dargyris <diamantisargiris@gmail.com>
2020-06-01 21:41:10 +0200

    Simple generic menu.

---
+++ X/Menu/Include.h
@@ -0,0 +1,10 @@
+#include <iostream>     // cin, cout, endl
+#include <unistd.h>     // usleep
+#include <string>       // strings
+#include <regex>        // regex
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::regex;
---
+++ X/Menu/Main.cpp
@@ -0,0 +1,39 @@
+#include "Include.h"
+#include "Utils.cpp"
+#include "Menu.cpp"
+#include "Funct.cpp"
+
+int main(){
+    system ( "clear" );
+
+    unsigned short threeTries {3};
+    bool mainFlag {true};
+    do {
+
+        char choice = printMenu( &threeTries );
+        switch ( choice ){
+            case '1':
+                threeTries = fun1();
+                break;
+            case '2':
+                threeTries = fun2();
+                break;
+            case '3':
+                threeTries = fun3();
+                break;
+            case '4':
+                threeTries = fun4();
+                break;
+            case 'Q':
+                mainFlag = quit();
+                break;
+            default:
+                mainFlag = fail( &threeTries );
+                break;
+        }
+        
+    } while ( mainFlag == true );
+
+    wait( 1 );
+    return 0;
+}
