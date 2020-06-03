53375fbe3710db79e6ed39b1e821ca44dfdce1c1
dargyris <diamantisargiris@gmail.com>
2020-06-03 17:46:39 +0200

    Demonstration of default values.

--- 03_Functions/DefaultVals/Main.cpp
+++ 03_Functions/DefaultVals/Main.cpp
@@ -1,28 +1,38 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
 
-double calc_cost( double base_cost, double tax_rate, double shipping );
+double calc_cost( double base_cost = 100., double tax_rate = 0.06, double shipping = 3.50 );
+void greeting( string name, string prefix = "Mr.", string suffix = "" );
 
 int main(){
+
+    cout << std::fixed << std::setprecision( 2 );
     system("clear");
 
-    double base_cost {};
-    double tax_rate {};
-    double base_shipping {};
+    double cost {};
+    cost = calc_cost( 100.0, 0.08, 4.25 );
+    cout << "Cost is: " << cost << endl;
 
-    cout << "Welcome!" << endl;
-    cout << "Base cost: ";
-    cin >> base_cost;
-    cout << "\nTax rate: ";
-    cin >> tax_rate;
-    cout << "\nBase shipping: ";
-    cin >> base_shipping;
+    cost = calc_cost(100.0, 0.08);
+    cout << "Cost is: " << cost << endl;
 
-    cout << calc_cost( base_cost, tax_rate, base_shipping ) << endl;
+    cost = calc_cost(200.0);
+    cout << "Cost is: " << cost << endl;
+
+    cost = calc_cost();
+    cout << "Cost is: " << cost << endl;
+
+    greeting( "Glenn Jones", "Dr.", "M.D." );
+    greeting( "James Rogers", "Professor", "Ph.D." );
+    greeting( "Frank Miller", "Dr." );
+    greeting( "william Smith" );
+    greeting( "Mary Howard", "Mrs.", "Ph.D." );
     
 
     fflush(stdin);
@@ -30,6 +40,10 @@ int main(){
     return 0;
 }
 
-double calc_cost( double base_cost, double tax_rate = 0.06, double shipping = 3.50 ){
-    return base_cost += ( base_cost * tax_rate );
+double calc_cost( double base_cost, double tax_rate, double shipping ){
+    return base_cost += ( base_cost * tax_rate ) + shipping;
+}
+
+void greeting( string name, string prefix, string suffix ){
+    cout << "Hello " << prefix + " " + name + " " + suffix << endl;
 }
