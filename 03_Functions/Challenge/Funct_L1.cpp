da12dbc7c6bc10bcd9e641101c5a0015d0f7e5e8
dargyris <diamantisargiris@gmail.com>
2020-06-05 16:34:39 +0200

    Adding level 1 functionality.

---
+++ 03_Functions/Challenge/Funct_L1.cpp
@@ -0,0 +1,54 @@
+unsigned short handlePrint( const vector<int> );
+void print( const vector<int> );
+unsigned short add( vector<int> );
+unsigned short mean( const vector<int> );
+double calculate_mean ( const vector<int> );
+unsigned short min( vector<int> );
+unsigned short max( vector<int> );
+
+unsigned short handlePrint( const vector<int> &num ){
+    if ( num.size() == 0 ){
+        cout << "[]: Empty list." << endl;
+    } else {
+        print(num);
+    }
+    return 3;   // Legal choice, restores threeTries to 3
+}
+
+void print( const vector<int> &num ){
+    cout << "{ ";
+    for ( auto v: num ){
+        cout << v << " ";
+    }
+    cout << "}" << endl;
+}
+
+unsigned short add( vector<int> &num ){
+    int num_to_add {};
+    cout << "Enter int to add > ";
+    cin >> num_to_add;
+    num.push_back( num_to_add );
+    cout << num_to_add << " added." << endl;
+    return 3;   // Legal choice, restores threeTries to 3
+}
+
+unsigned short mean( const vector<int> &num ){
+    if ( num.size() == 0 ){
+        cout << "[]: Empty list." << endl;
+    } else {
+        cout << "Mean\t: " << calculate_mean( num ) << endl;
+    }
+    return 3;   // Legal choice, restores threeTries to 3
+}
+
+void calculate_mean (  ){
+    
+}
+
+unsigned short min( vector<int> num ){
+    return 3;   // Legal choice, restores threeTries to 3
+}
+
+unsigned short max( vector<int> num ){
+    return 3;   // Legal choice, restores threeTries to 3
+}
