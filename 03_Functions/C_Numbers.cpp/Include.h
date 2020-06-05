ed818a24c89b85e0eec7c775d727bd9a8773080b
dargyris <diamantisargiris@gmail.com>
2020-06-05 18:59:08 +0200

    Housekeeping.

---
+++ 03_Functions/C_Numbers.cpp/Include.h
@@ -0,0 +1,14 @@
+#include <iostream>     // cin, cout, endl
+#include <unistd.h>     // usleep
+#include <string>       // strings
+#include <regex>        // regex
+#include <vector>       // vectors
+#include <string>       // strings
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::regex;
+using std::vector;
+using std::string;
