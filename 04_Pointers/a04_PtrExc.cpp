8767dc803a7bb26b964190375d2d53468533ec46
dargyris <diamantisargiris@gmail.com>
2020-06-07 20:56:16 +0200

    Simple pointer demo.

---
+++ 04_Pointers/a04_PtrExc.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+    system ( "clear" );
+
+    short *myShortPtr { nullptr };
+    int *myIntPtr { nullptr };
+    
+
+    fflush ( stdin );
+    cin.get();
+    return 0;
+}
