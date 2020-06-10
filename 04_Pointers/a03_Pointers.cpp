f578291d8eb821abd7284ecd88c1a5cc9211cd49
dargyris <diamantisargiris@gmail.com>
2020-06-10 14:53:47 +0200

    Testing Vim improvements.

--- 04_Pointers/a03_Pointers.cpp
+++ 04_Pointers/a03_Pointers.cpp
@@ -6,10 +6,31 @@ using std::endl;
 
 int main(){
     system ( "clear" );
+    
+    cout << "\tVar\tValue\t\tAddress\t\tType\tSize" << endl;
+    cout << "\t----------------------------------------------------" << endl; 
+    int a {5};
+    cout << "\ta{5}\t" << a << "\t\t" << &a << "\t" 
+         << typeid(a).name() << "\t" << sizeof(a) << endl;
 
+    int *A {&a};
+    cout << "\tA{&a}\t" << A << "\t" << &A << "\t"
+         << typeid(A).name() << "\t" << sizeof(A) << endl;
+
+    int b {*A};
+    cout << "\tb{*A}\t" << b << "\t\t" << &b << "\t"
+         << typeid(a).name() << "\t" << sizeof(b) << endl;
     
 
     fflush ( stdin );
     cin.get();
     return 0;
 }
+
+Some text(text) here
+Some text(text) here
+Some text(text) here
+Some text ( text ) here
+
+Some text here
+
